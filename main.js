ebf59de550017ed4f8e571159bfe6535fdb3bf74
merrycrh <malikaabdukarimova15@gmail.com>
2024-05-12 19:28:31 +0500

    done snowflake effect with icons

--- main.js
+++ main.js
@@ -1,50 +1,64 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const nameInput = document.querySelector(".input1");
-  const ageInput = document.querySelector(".input2");
-  const marriedInput = document.querySelector(".input3");
-  const outputDiv = document.querySelector(".output2");
-  const submitBtn = document.querySelector(".btn1");
-  const cancelBtn = document.querySelector(".btn2");
+setInterval(createTelegram, 100)
+setInterval(createInstagram, 100)
+setInterval(createTwitter, 100)
+setInterval(createYoutube, 100)
 
-  const displayInput1 = () => {
-    const name = nameInput.value;
+function createTelegram() {
+    const telegram = document.createElement("i");
+    telegram.classList.add("fa-brands");
+    telegram.classList.add("fa-telegram");
 
-    document.getElementById("name").textContent = "Name: " + name;
-  };
+    telegram.style.left = Math.random() * window.innerWidth + "px";
+    telegram.style.animationDuration = Math.random() * 3 +2 + "s";
+    telegram.style.opacity = Math.random();
+    document.body.append(telegram);
 
-  const displayInput2 = () => {
-    const age = ageInput.value;
+    setTimeout(() => {
+        telegram.remove();
+    }, 4000);
+}
 
-    document.getElementById("age").textContent = "Age: " + age;
-  };
+function createInstagram() {
+    const instagram = document.createElement("i");
+    instagram.classList.add("fa-brands");
+    instagram.classList.add("fa-instagram");
 
-  const displayInput3 = () => {
-    const isMarried = marriedInput.value;
+    instagram.style.left = Math.random() * window.innerWidth + "px";
+    instagram.style.animationDuration = Math.random() * 3 +2 + "s";
+    instagram.style.opacity = Math.random();
+    document.body.append(instagram);
 
-    document.getElementById("married").textContent = "Is Married: " + isMarried;
-  };
+    setTimeout(() => {
+        instagram.remove();
+    }, 4000);
+}
 
-  nameInput.addEventListener("keyup", displayInput1);
-  ageInput.addEventListener("keyup", displayInput2);
-  marriedInput.addEventListener("keyup", displayInput3);
+function createTwitter() {
+    const twitter = document.createElement("i");
+    twitter.classList.add("fa-brands");
+    twitter.classList.add("fa-square-twitter");
 
-  const displaySubmission = () => {
-    const submissionText = document.createElement("p");
-    submissionText.textContent = "Yuborildi";
-    outputDiv.appendChild(submissionText);
-  };
+    twitter.style.left = Math.random() * window.innerWidth + "px";
+    twitter.style.animationDuration = Math.random() * 3 +2 + "s";
+    twitter.style.opacity = Math.random();
+    document.body.append(twitter);
 
-  submitBtn.addEventListener("click", function () {
-    displayInput1();
-    displayInput2();
-    displayInput3();
-    displaySubmission();
-  });
+    setTimeout(() => {
+        twitter.remove();
+    }, 4000);
+}
 
-  cancelBtn.addEventListener("click", function () {
-    nameInput.value = "";
-    ageInput.value = "";
-    marriedInput.value = "";
-    outputDiv.innerHTML = "";
-  });
-});
+function createYoutube() {
+    const youatube = document.createElement("i");
+    youatube.classList.add("fa-brands");
+    youatube.classList.add("fa-youtube");
+
+    youatube.style.left = Math.random() * window.innerWidth + "px";
+    youatube.style.animationDuration = Math.random() * 3 +2 + "s";
+    youatube.style.opacity = Math.random();
+    document.body.append(youatube);
+
+    setTimeout(() => {
+        youatube.remove();
+    }, 4000);
+}
